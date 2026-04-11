b38a286589b8c29dad91733e76e14232472979ad
Liam Mefus <liam.mefus@outlook.de>
2026-04-11 11:43:25 +0200

    add(db): plant notes

---
+++ src/Entity/PlantNotes.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlantNotesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlantNotesRepository::class)]
+class PlantNotes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at;
+
+    public function __construct()
+    {
+        // update the timestamps from the same object to have the exact same times
+        // there would be a small difference in the nanoseconds otherwise
+        $date = new \DateTimeImmutable();
+        $this->created_at = $date;
+        $this->updated_at = $date;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
