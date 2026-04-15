e62f9a824af66b0dbc3440b225253e4da0ea5ded
Liam Mefus <liam.mefus@outlook.de>
2026-04-15 16:50:53 +0200

    add(Fixtures): test data for users, plants and locations

---
+++ src/DataFixtures/PlantFixture.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Plants;
+use App\Entity\User;
+use App\Enum\HumidityRequirement;
+use App\Enum\LightRequirement;
+use App\Enum\TemperatureRequirement;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PlantFixture extends Fixture implements DependentFixtureInterface, FixtureGroupInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $plant = new Plants();
+            $plant->setName("Testplant-" . $i);
+            $plant->setDescription("Description");
+            $plant->setBotanicalName("BotanicalName");
+            $plant->setLightRequirement(LightRequirement::halfshady);
+            $plant->setTemperatureRequirement(TemperatureRequirement::cool);
+            $plant->setHumidityRequirement(HumidityRequirement::medium);
+            $plant->setSoilType("TestSoilType");
+            $plant->setPotSize("20 cm");
+            $plant->setLastFertilizedAt(new \DateTimeImmutable());
+            $plant->setLastRepottedAt(new \DateTimeImmutable());
+            $plant->setLastWateredAt(new \DateTimeImmutable());
+            $plant->setWateringIntervalDays(15);
+            $plant->setFertilizingIntervalDays(51);
+            $plant->setRepottingIntervalDays(465);
+            $plant->setToxicForAnimals(false);
+            $plant->setToxicForHumans(false);
+            $plant->setPurchaseDate(new \DateTimeImmutable());
+            $plant->setStressScore(100);
+            $plant->setUser($this->getReference(UserFixture::TEST_USER, User::class));
+
+            $manager->persist($plant);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixture::class,
+        ];
+    }
+
+    public static function getGroups(): array
+    {
+        return [
+            'test_data'
+        ];
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,5 +17,5 @@ passthru('php bin/console doctrine:database:drop -f --if-exists --env=test');
 passthru('php bin/console doctrine:database:create --if-not-exists --env=test');
 // get done with the migration processes
 passthru('echo yes | php bin/console doctrine:migrations:migrate --env=test --no-interaction');
-
-passthru('php bin/console doctrine:fixtures:load --env=test --no-interaction');
+// we only need the user to be loaded, usually the tests need specific data to be tested againsts
+passthru('php bin/console doctrine:fixtures:load --group=users --env=test --no-interaction');
